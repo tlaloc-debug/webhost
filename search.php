b9e748dae1a41b7eb2feccfd4541919ac847d3d1
tlaloc_debug <pelondinho@hotmail.com>
2021-02-24 20:47:09 -0500

    add responsive design

--- search.php
+++ search.php
@@ -1,34 +1,155 @@
-
-<table>
-<tr>
-    <td>
-        <label>Clear Results</label>
-    </td>
-    <td>
-        <input type='checkbox' onclick='myFunction1()' checked>Prog memory</button>
-        <input type='checkbox' onclick='myFunction2()' checked>Memory Type</button>
-        <input type='checkbox' onclick='myFunction3()' checked>Eeprom</button><br>
-        <input type='checkbox' onclick='myFunction4()' checked>RAM</button>
-        <input type='checkbox' onclick='myFunction5()' checked>I/O Pins</button>
-        <input type='checkbox' onclick='myFunction6()' checked>Pckages</button>
-    </td>
-    <td>
-        <input type='checkbox' onclick='myFunction7()' checked>ADC</button>
-        <input type='checkbox' onclick='myFunction8()' checked>Comp</button>
-        <input type='checkbox' onclick='myFunction9()' checked>Timers</button>
-        <input type='checkbox' onclick='myFunction10()' checked>Serial Comm</button><br>
-        <input type='checkbox' onclick='myFunction11()' checked>Max Speed</button>
-        <input type='checkbox' onclick='myFunction12()' checked>IntOSC</button>
-    </td>
-</tr>
-</table>
+<div class="main">
+  <div class="content" id="one">
+    <div id="clearpc">Clear Results:</div>
+  </div> 
+  <div class="content" id="two">
+    <input type='checkbox' onclick='myFunction1()' checked>Prog memory</button>
+    <input type='checkbox' onclick='myFunction2()' checked>Memory Type</button>
+    <input type='checkbox' onclick='myFunction3()' checked>Eeprom</button><br>
+    <input type='checkbox' onclick='myFunction4()' checked>RAM Module</button>
+    <input type='checkbox' onclick='myFunction5()' checked>In - Out Pins</button>
+    <input type='checkbox' onclick='myFunction6()' checked>Packages</button>
+  </div>  
+  <div class="content" id="three">
+    <input type='checkbox' onclick='myFunction7()' checked>An/Dig Conv.</button>
+    <input type='checkbox' onclick='myFunction8()' checked>Comparators</button>
+    <input type='checkbox' onclick='myFunction9()' checked>Timers </button><br>
+    <input type='checkbox' onclick='myFunction10()' checked>Serial Comm</button>
+    <input type='checkbox' onclick='myFunction11()' checked>Max Frequency</button>
+    <input type='checkbox' onclick='myFunction12()' checked>Internal OSC</button>
+  </div>
+</div>
 
 <?php
 
+$monitor = $_POST["screen"];
+
+if ($monitor>400) {
 echo "<style>
-th, td {border: 1px solid black;}
-tr:hover {background-color:#f5f5f5;}
+
+.main {
+  width: 1700px;
+  height: 100px;
+  display: flex;
+  background-color: white;
+  position: sticky;
+  top: 0px;
+  z-index: +1;
+}
+
+.content {
+  margin: 20px 0px;
+}
+
+#clearpc {
+  padding-top: 10px;
+}
+
+#one {
+  position: sticky;
+  left: 20px;
+}
+
+#two {
+  position: sticky;
+  left: 180px;
+}
+
+#three {
+  position: sticky;
+  left: 620px;
+}
+
+th, td {
+  border: 1px solid black;
+}
+
+tr:hover {
+  background-color:#f5f5f5;
+}
+
+#item {
+  position: sticky;
+  left: 0px;
+  background-color: white;
+}
+
+#item1 {
+  position: sticky;
+  left: 0px;
+  background-color: white;
+  z-index: +1;
+}
+
+#item1, #memory1, #type1, #eeprom1, #ram1, #pins1, #box1, #adc1, #comp1, #timers1, #serial1, #max1, #intosc1  {
+  position: sticky;
+  top: 100px;
+  background-color: white;
+}
+
 </style>";
+} else {
+  echo "<style>
+  
+  .main {
+    width: 1700px;
+    height: 170px;
+    display: flex;
+    background-color: white;
+    position: sticky;
+    top: 0px;
+    z-index: +1;
+  }
+  
+  .content {
+    margin: 20px 0px;
+  }
+  
+  #one {
+    display: none;
+  }
+  
+  #two {
+    position: sticky;
+    left: 10px;
+    width: 125px;
+  }
+  
+  #three {
+    position: sticky;
+    left: 150px;
+    width: 125px;
+  }
+  
+  th, td {
+    border: 1px solid black;
+  }
+  
+  tr:hover {
+    background-color:#f5f5f5;
+  }
+  
+  #item {
+    position: sticky;
+    left: 0px;
+    background-color: white;
+  }
+  
+  #item1 {
+    position: sticky;
+    left: 0px;
+    background-color: white;
+    z-index: +1;
+  }
+  
+  #item1, #memory1, #type1, #eeprom1, #ram1, #pins1, #box1, #adc1, #comp1, #timers1, #serial1, #max1, #intosc1  {
+    position: sticky;
+    top: 170px;
+    background-color: white;
+  }
+ 
+</style>";
+}
 
 $i = 2;
 $servername = "localhost";
@@ -48,12 +169,12 @@ $busqueda = $_POST["fname"];
 $sql = "SELECT * FROM micros, analog, digital, speeds, memorytype, presentation where product like '%$busqueda%' and micros.micro_id=analog.adc_id and micros.micro_id=digital.dig_id and micros.micro_id=speeds.speed_id and micros.packages=presentation.box_id and micros.memorytype=memorytype.type_id order by micro_id";
 $result = mysqli_query($conn, $sql);
 
-echo "<table><tr><th>Product</th><th id='memory1'>Memory</th><th id='type1'>Memory_type</th><th id='eeprom1'>EEprom</th><th id='ram1'>RAM</th><th id='pins1'>I/O_pins</th><th style='padding-right:160px;' id='box1'>Packages</th><th id='adc1'>A/D_Converter</th><th id='comp1'>Comp</th><th style='padding-right:100px;' id='timers1'>Timers</th><th style='padding-right:150px;' id='serial1'>Serial_Comm</th><th id='max1'>Max_Speed</th><th style='padding-right:100px;' id='intosc1'>IntOSC</th></tr>";
+echo "<table id='thistable'><tr><th id='item1'>Product</th><th id='memory1'>Memory</th><th id='type1'>Memory_type</th><th id='eeprom1'>EEprom</th><th id='ram1'>RAM</th><th id='pins1'>I/O_pins</th><th style='padding-right:160px;' id='box1'>Packages</th><th id='adc1'>A/D_Converter</th><th id='comp1'>Comp</th><th style='padding-right:100px;' id='timers1'>Timers</th><th style='padding-right:150px;' id='serial1'>Serial_Comm</th><th id='max1'>Max_Speed</th><th style='padding-right:100px;' id='intosc1'>IntOSC</th></tr>";
 
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo "<tr><td>" . $row["product"]. "</td><td id='memory$i'>" . $row["progmemory"]. "</td><td id='type$i'>" . $row["memtype"]. "</td><td id='eeprom$i'>" . $row["eeprom"]. "</td><td id='ram$i'>" . $row["ram"]. "</td><td id='pins$i'>" . $row["pins"]. "</td><td id='box$i'>" . $row["box"]. "</td><td id='adc$i'>" . $row["ADC"] . " x " . $row["res"]. "bit </td><td id='comp$i'>" . $row["comp"]. "</td><td id='timers$i'>" . $row["timer16"]. "-16bit, " . $row["timer8"]. "-8bit </td><td id='serial$i'>" . $row["serial"]. "</td><td id='max$i'>" . $row["max"]. " MHz </td><td id='intosc$i'>" . $row["intOSC"]. " MHz </td></tr>";
+        echo "<tr><td id='item'>" . $row["product"]. "</td><td id='memory$i'>" . $row["progmemory"]. "</td><td id='type$i'>" . $row["memtype"]. "</td><td id='eeprom$i'>" . $row["eeprom"]. "</td><td id='ram$i'>" . $row["ram"]. "</td><td id='pins$i'>" . $row["pins"]. "</td><td id='box$i'>" . $row["box"]. "</td><td id='adc$i'>" . $row["ADC"] . " x " . $row["res"]. "bit </td><td id='comp$i'>" . $row["comp"]. "</td><td id='timers$i'>" . $row["timer16"]. "-16bit, " . $row["timer8"]. "-8bit </td><td id='serial$i'>" . $row["serial"]. "</td><td id='max$i'>" . $row["max"]. " MHz </td><td id='intosc$i'>" . $row["intOSC"]. " MHz </td></tr>";
 $i++;
     }
 echo "</table>";
